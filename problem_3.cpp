18758f58f107066a47d3c25359e6c3499eb5cecf
czcas <157773148+czcas@users.noreply.github.com>
2024-01-31 03:54:25 +0000

    Pending changes exported from your codespace

---
+++ problem_3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    double x = 0;
+    cout << "enter a double: ";
+    cin >> x;
+    cout << " Your double is:  " << x * x << "";
+    return 0;
+}
